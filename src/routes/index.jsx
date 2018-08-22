1754194823fccca38e47aed4aa8c6a26ad31d8ae
Edd <eddsilva@gmail.com>
2018-08-21 22:53:34 -0300

    inject styled component global theme

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Theme } from 'core/constants/default.theme'
+import { GlobalStyle } from 'core/assets/style/global'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Main from 'containers/Main'
 
 const Routes = () => {
+  GlobalStyle(Theme)
+
   return (
-    <Router>
-      <Route exact patch='/' component={ Main } />
-    </Router>
+    <ThemeProvider theme={Theme}>
+      <Router>
+        <Route exact patch='/' component={ Main } />
+      </Router>
+    </ThemeProvider>
   )
 }
 
